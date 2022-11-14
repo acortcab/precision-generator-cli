fb0ceaa8302fdc5176e276d1b6eb3650e1f06b7c
ev8 <ev8@qad.com>
2022-11-14 13:34:41 +0100

    some modifications

--- ask.js
+++ ask.js
@@ -1,7 +1,7 @@
 #! /usr/bin/env node
 const path = require('path');
 const fs = require('fs-extra')
-const fileSettings = 'C:\\\maven\\apache-maven-3.8.6\\conf\\settings.xml';
+const fileSettings = 'C:\\\Users\\ev8\\.m2\\settings.xml';
 const mvn = require('maven').create({'settings':fileSettings});
 var define ={};
 const pluginMaven = 'archetype:generate';
@@ -10,6 +10,8 @@ var initial = false;
 // directory to check if exists
 const dirOld = './te'
 const dirGenerated = './generated'
+var runAll = false;
+var dirMove = "C:\\Workspace\\workspace-microservices\\qad-gtte-webui-te";
 
 this.inialize=function (activeDebug){
     define['archetypeGroupId'] = 'precisionsoftware';
@@ -17,6 +19,10 @@ this.inialize=function (activeDebug){
     define['artifactId'] = 'te';
     define['version'] = '1.0-SNAPSHOT';
     define['interactiveMode'] = 'false';
+    if(activeDebug != null && activeDebug == '--all'){
+        runAll = true;
+        define['archetypeArtifactId'] = 'precision-generator-all';
+    }
     if(activeDebug != null && (activeDebug == '-d' || activeDebug == '--debug')){
         define['interactiveMode'] = 'true';
     }
@@ -26,36 +32,40 @@ this.inialize=function (activeDebug){
 
 this.askEverything=function (activeDebug){
     ask.inialize(activeDebug);
-
-    const inquirer = require('inquirer');
-    option = null;
-    inquirer
-      .prompt([
-        {
-            type: 'list',
-            name: 'action',
-            message: 'What would like to create?',
-            choices: ['repository', 'entity', 'form', 'validator'],
-        }
-      ])
-      .then(answers => {
-        
-        switch (answers.action){
-            case 'repository':
-                ask.askQuestionsRepository(activeDebug);
-                return;
-            case 'entity':
-                ask.askQuestionsEntity(activeDebug);
-                return;
-            case 'form':
-                ask.askQuestionsForm(activeDebug);
-                return;
-            case 'validator':
-                ask.askQuestionsValidator(activeDebug);
-                return;
-        }
-        
-      });
+	
+	if(activeDebug == null || activeDebug != '--all'){
+		const inquirer = require('inquirer');
+		option = null;
+		inquirer
+		  .prompt([
+			{
+				type: 'list',
+				name: 'action',
+				message: 'What would like to create?',
+				choices: ['repository', 'entity', 'form', 'validator'],
+			}
+		  ])
+		  .then(answers => {
+			
+			switch (answers.action){
+				case 'repository':
+					ask.askQuestionsRepository(activeDebug);
+					return;
+				case 'entity':
+					ask.askQuestionsEntity(activeDebug);
+					return;
+				case 'form':
+					ask.askQuestionsForm(activeDebug);
+					return;
+				case 'validator':
+					ask.askQuestionsValidator(activeDebug);
+					return;
+			}
+			
+		  });
+	}else{
+		ask.askQuestionsRepository(activeDebug);
+	}
 }
 
 this.askQuestionsRepository=function (activeDebug){
@@ -69,34 +79,39 @@ this.askQuestionsRepository=function (activeDebug){
       .prompt([
         {
           name: 'packageInPathFormat',
-          message: 'Which package you want to locate the class?',
+          message: 'Which package you want to locate the repository?',
           default: 'com.qad.gtte.te',
         },
 	    {
-          name: 'className',
-          message: 'How will you class be named?',
-          default: 'testClass',
+          name: 'repositoryName',
+          message: 'How will your repository be named?',
+          default: 'testRepository',
         },
-		    {
+		{
           name: 'classObject',
-          message: 'How will you Object be named?',
-          default: 'testObject',
+          message: 'Which domain will you use?',
+          default: 'testDomain',
         },
 	    {
           name: 'classId',
-          message: 'How will you class ID be named?',
+          message: 'Which domain ID will you use?',
           default :'testId',
         },
       ])
       .then(answers => {
-	        define['archetypeArtifactId'] = 'precision-generator-repository';
+            if(!runAll)
+	            define['archetypeArtifactId'] = 'precision-generator-repository';
             define['groupId'] = answers.packageInPathFormat;
-            define['className'] = answers.className;
+            define['repositoryName'] = answers.repositoryName;
             define['classObject'] = answers.classObject;
             define['classId'] = answers.classId;
-            mvn.execute(pluginMaven, define).then(() => {
-                ask.moveContent();
-             });
+            if(!runAll){
+                mvn.execute(pluginMaven, define).then(() => {
+                    ask.moveContent();
+                 });
+            }else{
+                ask.askQuestionsEntity(activeDebug);
+            }
       });
 }
 
@@ -106,35 +121,38 @@ this.askQuestionsEntity=function (activeDebug){
   }
 
   const inquirer = require('inquirer');
-  define['archetypeArtifactId'] = 'precision-generator-entity';
 
     inquirer
       .prompt([
         {
           name: 'packageInPathFormat',
-          message: 'Which package you want to locate the class?',
-          default: 'com.qad.gtte',
+          message: 'Which package you want to locate the entity?',
+          default: 'com.qad.gtte.te',
+		  when: runAll == false,
         },
 	    {
-          name: 'className',
-          message: 'How will you class be named?',
-          default: 'testClass',
+          name: 'entityName',
+          message: 'How will you entity be named?',
+          default: 'testDomain',
         },
 		{
           name: 'tableName',
-          message: 'How will you table mapped?',
+          message: 'Which table will you map?',
           default: 'testTable',
         }
       ])
       .then(answers => {
-            define['groupId'] = answers.packageInPathFormat;
-            define['className'] = answers.className;
+            if(!runAll){
+                define['archetypeArtifactId'] = 'precision-generator-entity';
+                define['groupId'] = answers.packageInPathFormat;
+            }
+            define['entityName'] = answers.entityName;
             define['tableName'] = answers.tableName;
-            ask.askQuestionsColumns();
+            ask.askQuestionsColumns(activeDebug);
       });
 }
 
-this.askQuestionsColumns=function (){
+this.askQuestionsColumns=function (activeDebug){
     const inquirer = require('inquirer');
 
     inquirer
@@ -170,9 +188,13 @@ this.askQuestionsColumns=function (){
                 counter++;
                 ask.askQuestionsColumns();
             }else{
-                mvn.execute(pluginMaven, define).then(() => {
-                    ask.moveContent();
-                });
+                if(!runAll){
+                    mvn.execute(pluginMaven, define).then(() => {
+                        ask.moveContent();
+                    });
+                }else{
+                    ask.askQuestionsForm(activeDebug);
+                }
             }
       });
 }
@@ -188,24 +210,27 @@ this.askQuestionsForm=function (activeDebug){
       .prompt([
         {
           name: 'packageInPathFormat',
-          message: 'Which package you want to locate the class?',
-          default: 'com.qad.gtte.te',
+          message: 'Which package you want to locate the form?',
+          default: 'com.precisionsoftware.dem.controller.incident',
+		  when: runAll == false,
         },
 	    {
-          name: 'className',
-          message: 'How will you class be named?',
-          default: 'testClass',
+          name: 'formName',
+          message: 'How will your form be named?',
+          default: 'testForm',
         }
       ])
       .then(answers => {
-            define['archetypeArtifactId'] = 'precision-generator-form';
-            define['groupId'] = answers.packageInPathFormat;
-            define['className'] = answers.className;
+            if(!runAll){
+                define['archetypeArtifactId'] = 'precision-generator-form';
+                define['groupId'] = answers.packageInPathFormat;
+            }
+            define['formName'] = answers.formName;
             ask.askQuestionsFormsField();
       });
 }
 
-this.askQuestionsFormsField=function (){
+this.askQuestionsFormsField=function (activeDebug){
     const inquirer = require('inquirer');
 
     inquirer
@@ -235,9 +260,13 @@ this.askQuestionsFormsField=function (){
                 counter++;
                 ask.askQuestionsFormsField();
             }else{
-                mvn.execute(pluginMaven, define).then(() => {
-                    ask.moveContent();
-                });
+                if(!runAll){
+                    mvn.execute(pluginMaven, define).then(() => {
+                        ask.moveContent();
+                    });
+                }else{
+                    ask.askQuestionsValidator(activeDebug);
+                }
             }
       });
 }
@@ -253,13 +282,14 @@ this.askQuestionsValidator=function (activeDebug){
       .prompt([
         {
           name: 'packageInPathFormat',
-          message: 'Which package you want to locate the class?',
-          default: 'com.qad.gtte.te',
+          message: 'Which package you want to locate your validator?',
+          default: 'com.qad.gtte.te.controller',
+		  when: runAll == false,
         },
 	    {
-          name: 'className',
-          message: 'How will you class be named?',
-          default: 'testClass',
+          name: 'validatorName',
+          message: 'How will you validator be named?',
+          default: 'testValidator',
         },
 		    {
           name: 'contraintName',
@@ -267,17 +297,19 @@ this.askQuestionsValidator=function (activeDebug){
           default: 'testConstraint',
         },
 	    {
-          name: 'formName',
-          message: 'Which form will you use?',
-          default :'testForm',
+          name: 'validateFieldName',
+          message: 'Which class do you want to validate?',
+          default :'testValidateField',
         },
       ])
       .then(answers => {
-            define['archetypeArtifactId'] = 'precision-generator-validator';
-            define['groupId'] = answers.packageInPathFormat;
-            define['className'] = answers.className;
+            if(!runAll){
+                define['archetypeArtifactId'] = 'precision-generator-validator';
+                define['groupId'] = answers.packageInPathFormat;
+            }
+            define['validatorName'] = answers.validatorName;
             define['contraintName'] = answers.contraintName;
-            define['formName'] = answers.formName;
+            define['validateFieldName'] = answers.validateFieldName;
             mvn.execute(pluginMaven, define).then(() => {
                 ask.moveContent();
             });
@@ -292,6 +324,40 @@ this.moveContent=function (){
         fs.removeSync(dirOld + '/pom.xml')
         fs.renameSync(dirOld , dirGenerated); 
     }
+
+    const inquirer = require('inquirer');
+
+      inquirer
+        .prompt([
+          {
+              type: 'confirm',
+              name: 'confirmation',
+              message: 'Would you like to copy the folder?',
+              default: false
+          },
+          {
+          name: 'dirToMove',
+          message: 'Where do you want to move?',
+          default: dirMove,
+		  when: (answers) => answers.confirmation,
+          },
+        ])
+        .then(answers => {
+
+          if(answers.confirmation){
+              // copy source folder to destination
+                fs.copy(dirGenerated, answers.dirToMove, function (err) {
+                    if (err){
+                        console.log('An error occured while copying the folder.')
+                        return console.error(err)
+                    }
+                    console.log('Copy completed!')
+                });
+          }else{
+             return;
+          }
+        });
+
 }
 
 this.askMore=function (){
--- index.js
+++ index.js
@@ -9,19 +9,19 @@ const dir = './generated'
 
 console.log(
     chalk.yellow(
-      figlet.textSync(' ------------', { horizontalLayout: 'full' })
+      figlet.textSync(' --------', { horizontalLayout: 'full' })
     )
   ); 
 
 console.log(
     chalk.yellow(
-      figlet.textSync('|PRECISION CLI |', { horizontalLayout: 'full' })
+      figlet.textSync('|GTTE CLI |', { horizontalLayout: 'full' })
     )
   );
 
   console.log(
     chalk.yellow(
-      figlet.textSync(' ------------', { horizontalLayout: 'full' })
+      figlet.textSync(' --------', { horizontalLayout: 'full' })
     )
   );   
 
@@ -30,7 +30,16 @@ if (fs.existsSync(dir)) {
   fs.removeSync(dir); 
 }
 
-if(!process.argv[2] || process.argv[2] == '-d' || process.argv[2] == '--debug') {
+if(!process.argv[2] && process.argv[2] == '-h' || process.argv[2] == '--help'){
+    console.log(chalk.green.bold('Usage:'))
+    console.log(chalk.green.bold('ge: to run the cli'))
+    console.log(chalk.green.bold('ge <repository,entity,form>: to run the option you want directly'))
+    console.log(chalk.green.bold('ge --all: to run all at the same time'))
+    console.log(chalk.green.bold('ge --debug: to interact with maven archetype'))
+return;
+}
+
+if(!process.argv[2] || process.argv[2] == '-d' || process.argv[2] == '--debug' || process.argv[2] == '--all') {
     ask.askEverything(process.argv[2]);
 }else {   
     var command = process.argv[2]; 
